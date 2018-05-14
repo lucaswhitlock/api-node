fe10d0e0b1dfc29608f8e28d73d0ec13ec4517cf
sosrole <sosrolegit@gmail.com>
2018-05-14 00:48:28 -0400

    Iniciando implementacão de validators

--- app/model/people-schema.js
+++ app/model/people-schema.js
@@ -4,8 +4,8 @@ const peopleSchema = new mongoose.Schema({
   photo: { type: String, required: true },
   name: { type: String, required: true },
   position: { type: String, required: true },
-  office: { type: Number, min: 8, required: true },
-  lab: { type: Number, min: 8, required: true },
+  office: { type: Number, min: 10, required: true },
+  lab: { type: Number, min: 10, required: true },
   email: { type: String, required: true }
 }, {
   collection: 'peoples',
